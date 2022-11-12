91aaea3c99eb25037c6be5ddd76cf882bcfe4003
Kai Glasenapp <kaimecki@gmail.com>
2022-11-12 16:06:02 +0100

    working

--- AirMonitor.ino
+++ AirMonitor.ino
@@ -5,21 +5,25 @@
  #include "ThingSpeak.h"
  #include <ESP8266WiFi.h>
  #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+ #include <Adafruit_BME280.h>
 
- int rxPin = D3;
- int txPin = D4;
+ int rxPin = D3;   //sds tx-pin
+ int txPin = D4;   //sds rx-pin
+
+
+ 
  const int sleeptime = 1000*60*30;
 
  SdsDustSensor sds(rxPin, txPin);
+ Adafruit_BME280 bme; // I2C
 
  const char* ssid = "WG_Fit_GmbH_EXT";   // your network SSID (name)
  const char* pass = "Liebdochwenduwillst18";   // your network password
 
  WiFiClient  client;
 
- unsigned long myChannelNumber = 942047;
- const char * myWriteAPIKey = "I3SVACTGQ7M2KPEB";
+ unsigned long myChannelNumber = 1933391;
+ const char * myWriteAPIKey = "X9L20E60JG38677Y";
 
  void setup() {
 
@@ -29,7 +33,8 @@
    // Wait for serial to initialize.
    while(!Serial) { }
 
-
+  Wire.begin(D6, D5); // Make sure you have D3 & D4 hooked up to the BME280
+  bme.begin();
    WiFi.mode(WIFI_STA);
    ThingSpeak.begin(client);  // Initialize ThingSpeak
 
@@ -78,12 +83,18 @@
      Serial.print(pm.pm25);
      Serial.print(", PM10 = ");
      Serial.println(pm.pm10);
+     Serial.print("Temp: ");
+     Serial.println(bme.readTemperature());
 
      // if you want to just print the measured values, you can use toString() method as well
      Serial.println(pm.toString());
 
      ThingSpeak.setField(1, pm.pm25);
      ThingSpeak.setField(2, pm.pm10);
+     ThingSpeak.setField(3, bme.readHumidity());
+     ThingSpeak.setField(4, bme.readTemperature());
+     ThingSpeak.setField(5, bme.readPressure());
+     
 
      int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
      if(x == 200){
